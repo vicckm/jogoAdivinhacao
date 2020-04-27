dffd67e6611eb5f864f5a8f9792899a79a3479ea
Victoria Marques <vicck.rj@gmail.com>
2020-04-27 12:49:24 -0300

    uncomment code and add readme.md

--- js/scripts.js
+++ js/scripts.js
@@ -9,79 +9,70 @@ var campoPalpite = document.querySelector('.campoPalpite');
 
 var contagemPalpites = 1;
 var botaoReinicio;
-var palpiteUsuario = parseInt(campoPalpite.value);  
-function conferirPalpite() {
-  
+campoPalpite.focus();
 
-  if (isNaN(palpiteUsuario)) {
-    console.log("ok")
-    // palpiteUsuario.textContent = "";
-    // palpites.textContent = "Digite um valor válido.";
-    // configFimDeJogo();
+function conferirPalpite() {
+  var palpiteUsuario = Number(campoPalpite.value);
+  if (contagemPalpites === 1) {
+    palpites.textContent = 'Palpites anteriores: ';
+  }
+  palpites.textContent += palpiteUsuario + ' ';
+
+  if (palpiteUsuario == numeroAleatorio) {
+    ultimoResultado.textContent = "Parabéns! Você acertou!";
+    ultimoResultado.style.color = "green";
+    baixoOuAlto.textContent = "";
+    configFimDeJogo();
+  } else if (contagemPalpites == 10) {
+    ultimoResultado.textContent = "FIM DE JOGO!";
+    baixoOuAlto.textContent = "";
+    configFimDeJogo();
+  } else {
+    ultimoResultado.textContent = "Errado!"
+    ultimoResultado.style.color = "red";
+    if (palpiteUsuario < numeroAleatorio) {
+      baixoOuAlto.textContent = "Seu palpite está muito baixo!";
+    } else if (palpiteUsuario > numeroAleatorio) {
+      baixoOuAlto.textContent = "Seu palpite está muito alto!"
+    }
   }
 
-//   if (contagemPalpites == 1) {
-//     palpites.textContent = "Palpites anteriores: ";
-//   }
+  contagemPalpites++;
+  campoPalpite.value = "";
+  campoPalpite.focus();
+}
 
-//   palpites.textContent += palpiteUsuario + " ";
+envioPalpite.addEventListener('click', conferirPalpite);
 
+function configFimDeJogo() {
+  campoPalpite.disabled = true;
+  envioPalpite.disabled = true;
+  botaoReinicio = document.createElement('button');
+  botaoReinicio.textContent = "Iniciar novo jogo";
+  document.querySelector('.reiniciarJogo').appendChild(botaoReinicio);
+  botaoReinicio.style.border = "3px solid #5E503F";
+  botaoReinicio.style.marginTop = "10px";
+  botaoReinicio.style.padding = "5px";
+  botaoReinicio.style.backgroundColor = "#C6AC8F";
+
+  botaoReinicio.addEventListener('click', reiniciarJogo);
+}
+
+function reiniciarJogo() {
+  contagemPalpites = 1;
+
+  var reiniciarPartida = document.querySelectorAll('.resultadoPalpites p');
+  for (var i = 0; i < reiniciarPartida.length ; i++) {
+    reiniciarPartida[i].textContent = "";
+  }
   
-//   if (palpiteUsuario == numeroAleatorio) {
-//     ultimoResultado.textContent = "Parabéns! Você acertou!";
-//     ultimoResultado.style.color = "green";
-//     baixoOuAlto.textContent = "";
-//     configFimDeJogo();
-//   } else if (contagemPalpites == 10) {
-//     ultimoResultado.textContent = "FIM DE JOGO!";
-//     baixoOuAlto.textContent = "";
-//     configFimDeJogo();
-//   } else {
-//     ultimoResultado.textContent = "Errado!"
-//     ultimoResultado.style.color = "red";
-//     if (palpiteUsuario < numeroAleatorio) {
-//       baixoOuAlto.textContent = "Seu palpite está muito baixo!";
-//     } else if (palpiteUsuario > numeroAleatorio) {
-//       baixoOuAlto.textContent = "Seu palpite está muito alto!"
-//     }
-//   }
+  campoPalpite.disabled = false;
+  envioPalpite.disabled = false;
+  campoPalpite.value = "";
+  campoPalpite.focus();
 
-//   contagemPalpites++;
-//   campoPalpite.value = "";
-//   campoPalpite.focus();
-// }
+  ultimoResultado.style.backgroundColor = '#C6AC8F';
 
-// envioPalpite.addEventListener('click', conferirPalpite);
-
-// function configFimDeJogo() {
-//   campoPalpite.disabled = true;
-//   envioPalpite.disabled = true;
-//   botaoReinicio = document.createElement('button');
-//   botaoReinicio.textContent = "Iniciar novo jogo";
-//   document.querySelector('.reiniciarJogo').appendChild(botaoReinicio);
-//   botaoReinicio.style.border = "3px solid #5E503F";
-//   botaoReinicio.style.marginTop = "10px";
-//   botaoReinicio.style.padding = "5px";
-//   botaoReinicio.style.backgroundColor = "#C6AC8F";
-
-//   botaoReinicio.addEventListener('click', reiniciarJogo);
-// }
-
-// function reiniciarJogo() {
-//   contagemPalpites = 1;
-
-//   var reiniciarPartida = document.querySelectorAll('.resultadoPalpites p');
-//   for (var i = 0; i < reiniciarPartida.length ; i++) {
-//     reiniciarPartida[i].textContent = "";
-//   }
-  
-//   campoPalpite.disabled = false;
-//   envioPalpite.disabled = false;
-//   campoPalpite.value = "";
-//   campoPalpite.focus();
-
-//   ultimoResultado.style.backgroundColor = '#C6AC8F';
-
-//   numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+  numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 }
 
---
+++ readme.md
@@ -0,0 +1,6 @@
+##Jogo de Adivinhação <br/><br/>
+> Esse é um jogo simples de adivinhação.
+
+![](/home/vic/Videos/jogoAdivinhacao.gif)  
+#### Ferramentas 
+* HTML, CSS e Javascript
